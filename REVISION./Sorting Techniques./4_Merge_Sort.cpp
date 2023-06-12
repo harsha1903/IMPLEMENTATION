46aefe9100ac30ecaa8bc077e82c70fcbbc11eb1
harsha1903 <83920371+harsha1903@users.noreply.github.com>
2023-06-12 12:59:23 +0530

    Update 4_Merge_Sort.cpp

--- REVISION./Sorting Techniques./4_Merge_Sort.cpp
+++ REVISION./Sorting Techniques./4_Merge_Sort.cpp
@@ -1,20 +1,46 @@
 #include<iostream>
 using namespace std;
 
-void insertionSort(int arr[], int n)
+void merge(int arr[], int l, int m, int r)
 {
-    for(int i=1;i<n;i++){
-        for(int j=i-1;j>=0;j--){
-            if(arr[j+1]>arr[j]){
-                break;
-            }
-            else{
-                swap(arr[j+1],arr[j]);
-            }
+    int n1=m-l+1,n2=r-m;
+    int left[n1],right[n2];
+    for(int i=0;i<n1;i++){
+        left[i]=arr[i+l];
+    }
+        
+    for(int i=0;i<n2;i++){
+        right[i]=arr[i+m+1];
+    }
+    
+    int i=0,j=0,index=l;
+    while(i<n1 && j<n2){
+        if(left[i]<=right[j]){
+            arr[index++]=left[i++];
         }
+        else{
+            arr[index++]=right[j++];
+        }
+    }
+    while(i<n1){
+        arr[index++]=left[i++];
+    }
+    while(j<n2){
+        arr[index++]=right[j++];
+    }
+}
+    
+void mergeSort(int arr[], int l, int r)
+{
+    if(l<r){
+        int mid=(l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
     }
 }
  
+
 int main(){
     int n;
     cin>>n;
@@ -22,7 +48,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    insertionSort(a,n);
+    mergeSort(a,0,n-1);
     for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
     }
