3b7ce2427dd5618e64f48dd5ca7a32ece7c24887
harsha1903 <83920371+harsha1903@users.noreply.github.com>
2023-06-12 12:48:58 +0530

    Update Selection Sort.cpp

--- REVISION./Sorting Techniques./Selection Sort.cpp
+++ REVISION./Sorting Techniques./Selection Sort.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[ ], int n)
+{
+    int min;
+    for(int i=0;i<n-1;i++){
+        min=i;
+        for(int j=i+1;j<n;j++){
+           if(arr[min]>arr[j])
+           {
+               min=j;
+           }
+        }
+       if(i!=min){ 
+           swap(arr[min],arr[i]);
+       }
+    }
+ }
+ 
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    selectionSort(a,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+} 
 
