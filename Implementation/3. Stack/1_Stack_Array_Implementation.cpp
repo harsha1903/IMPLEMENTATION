b9c843c2aeaa3a2a361f5a13bdcc46c33b1fe064
harsha1903 <83920371+harsha1903@users.noreply.github.com>
2023-06-16 13:59:54 +0530

    Create 1_Stack_Array_Implementation.cpp

---
+++ Implementation/3. Stack/1_Stack_Array_Implementation.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+struct stack{
+  int *arr;// we can also implement using vector
+  int top;
+  int cap;
+  
+  stack(int cap){
+      this->cap=cap;
+      top=-1;
+      arr=new int[cap];
+  }
+  
+  void push(int x){
+      top++;
+      if(top>=cap){
+          cout<<"Over Flow"<<endl;
+          return;
+      }
+      arr[top]=x;
+  }
+  
+  int pop(){
+    if(top==-1){
+      return -1;
+    }
+    int res=arr[top];
+    top--;
+    return res;
+  }
+  
+  int peek(){
+      if(top==-1){
+          return -1;
+      }
+      return arr[top];
+  }
+  
+  int size(){
+      return top+1;
+  }
+  
+  bool empty(){
+      if(top==-1) return 1;
+      return 0;
+  }
+  
+  void freeUp(){
+      delete[] arr;
+  }
+};
+
+int main()
+{
+    int cap,data;
+    cin>>cap;
+    stack s(cap);
+    for(int i=0;i<cap-1;i++){
+        cin>>data;
+        s.push(data);
+    }
+    cout<<s.peek()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.peek()<<endl;
+    cout<<s.size()<<endl;
+    cout<<s.empty()<<endl;
+    s.freeUp();
+    return 0;
+}
