ae8e024919bb04abde5fd2b9ae6ce863beb37983
Jammigumpula Sai Sriharsha <83920371+harsha1903@users.noreply.github.com>
2024-10-04 16:21:13 +0530

    Update 3_Insertion_Sort.cpp

--- Implementation/1. Sorting Techniques/3_Insertion_Sort.cpp
+++ Implementation/1. Sorting Techniques/3_Insertion_Sort.cpp
@@ -4,14 +4,14 @@ using namespace std;
 void insertionSort(int arr[], int n)
 {
     for(int i=1;i<n;i++){
-        for(int j=i-1;j>=0;j--){
-            if(arr[j+1]>arr[j]){
-                break;
-            }
-            else{
-                swap(arr[j+1],arr[j]);
-            }
+        int key = arr[i];
+        int j = i-1;
+        
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j=j-1;
         }
+        arr[j+1] = key;
     }
 }
  
